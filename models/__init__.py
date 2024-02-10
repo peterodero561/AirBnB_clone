ed1d112a7caf370415ca8bc8aea17c5188214564
Peter Odero <peterodero561@gmail.com>
2024-02-10 12:54:55 +0000

    perf: ln4: removed a commented code

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''init.py file containing BaseModel class for package'''
 import models
-#from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
