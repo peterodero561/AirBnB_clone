ff8ffd4b896c3ea3fec5993b9815aef365ca5c31
Peter Odero <peterodero561@gmail.com>
2024-02-10 06:51:45 +0000

    feat: all: added code to create a unique FileStorage instance of my application

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 '''init.py file containing BaseModel class for package'''
-from .base_model import BaseModel
+import models
+#from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
