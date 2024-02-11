11e389154418d2a1daf366324afed5296943ca8d
baragu <baraguantonyy@gmail.com>
2024-02-11 20:08:14 +0300

    changes

--- console.py
+++ console.py
@@ -3,12 +3,24 @@
 
 """ Console module """
 import cmd
+from models.base_model import BaseModel
 
 
-class HBNBcommand(cmd.Cmd):
+class HBNBcommand(cmd.Cmd, BaseModel):
     """ Console class to handle the input and output """
     #    intro = "Welcome to HBNB console!\nType help or ? to list commands.\n"
     prompt = "(hbnb)"
+
+    def check_class(self, line):
+        """
+        checks if class exists
+        """
+        try:
+            globals()[line]
+            return True
+        except (NameError, KeyError):
+            print("** class doesn't exist **")
+            return False
 
     def do_EOF(self, line):
         """ Perform action when reaching end of line """
@@ -18,6 +30,27 @@ class HBNBcommand(cmd.Cmd):
         """ Quit command to exit the program """
         #   print("Exiting ...")
         return True
+
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file) and 
+        prints the id
+        """
+        if line == "":
+            print("** class name missing **")
+
+        if self.check_class(line):
+            new_instance = globals()[line]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, line):
+        """
+        Print string representation of instance based on class name
+        and id
+        """
+
+
 
 
 if __name__ == "__main__":
